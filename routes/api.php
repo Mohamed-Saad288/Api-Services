0f84b8ca8a86ae9b805a6a9a2b45ae975177e825
Mohamed Saad <131921827+Mohamed-Saad288@users.noreply.github.com>
2024-04-02 17:15:56 +0300

    Connect With Database

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('test-api',function (){
+    return ['message' => 'Hello World'];
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
